9e97b876c503d8c4f5c4658e24f920d9c3afa7ea
jakobw10 <jakobw@udel.edu>
2021-10-21 17:34:56 -0400

    added claer button and setter hooks

--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Course from "../Interfaces/Course";
 export function Semester(props:{year :number,season:string,courses:Course[]}): JSX.Element{
+    const[courses,SetSemester]=useState(props.courses);
+    const[course,SetCourse]=useState(null);
+    let i:number;
+    function clearSemester(){
+        for(i=0;courses[i]==null;i++){
+            SetCourse(null);
+        }
+    }
     return <div className={"semester"}>
-        <div>Year {props.year} {props.season} Semester </div>
+        <div>Year {props.year} {props.season} Semester  <button onClick={clearSemester}>Clear</button></div>
         <div>Courses:
         </div>
-        <p>{props.courses[0].name}    Credits:{props.courses[0].credits}</p>
-        <p>{props.courses[1].name}    Credits:{props.courses[1].credits}</p>
+        <p>{courses[0].name}    Credits:{courses[0].credits}</p>
+        <p>{courses[1].name}    Credits:{courses[1].credits}</p>
     </div>
     ;
 }
