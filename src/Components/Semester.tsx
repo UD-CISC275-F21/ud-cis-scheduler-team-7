65bc542c0e915cbfc8c1d3507efac2a82b95bdc5
jakobw10 <jakobw@udel.edu>
2021-10-21 17:38:29 -0400

    added year clear button for testing

--- src/Components/Semester.tsx
+++ src/Components/Semester.tsx
@@ -3,14 +3,18 @@ import Course from "../Interfaces/Course";
 export function Semester(props:{year :number,season:string,courses:Course[]}): JSX.Element{
     const[courses,SetSemester]=useState(props.courses);
     const[course,SetCourse]=useState(null);
+    const[year,SetYear]=useState(props.year);
     let i:number;
     function clearSemester(){
         for(i=0;courses[i]==null;i++){
             SetCourse(null);
         }
     }
+    function clearYear(){
+        SetYear(0);
+    }
     return <div className={"semester"}>
-        <div>Year {props.year} {props.season} Semester  <button onClick={clearSemester}>Clear</button></div>
+        <div>Year {year} {props.season} Semester  <button onClick={clearSemester}>Clear</button><button onClick={clearYear}></button></div>
         <div>Courses:
         </div>
         <p>{courses[0].name}    Credits:{courses[0].credits}</p>
